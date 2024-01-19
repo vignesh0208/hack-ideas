ad2eeb5de8494460aa50989767b5c541a3f281c6
vignesh0208 <vigneshwaran@vajro.com>
2024-01-19 13:33:02 +0530

    Component and tailwind css
    Input and Button Component created and tailwind css installed

---
+++ src/Component/Button.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Button = ({
+  ButtonType,
+  handleClick,
+  extraClassName,
+  isDisabled,
+  children,
+}) => {
+  return (
+    <button
+      type={ButtonType}
+      onClick={handleClick}
+      className={`text-[#262626] text-sm rounded py-2 px-6 cursor-pointer border-[1px] border-[#c5c9cd] bg-white  ${extraClassName}`}
+      disabled={isDisabled}>
+      {children}
+    </button>
+  );
+};
+
+Button.propTypes = {
+  ButtonType: PropTypes.oneOf(['button', 'submit']).isRequired,
+  extraClassName: PropTypes.string,
+  isDisabled: PropTypes.bool,
+  children: PropTypes.any,
+  handleClick: PropTypes.func,
+};
+
+Button.defaultProps = {
+  ButtonType: 'button',
+  extraClassName: '',
+  isDisabled: false,
+  children: '',
+  handleClick: () => {},
+};
+
+export default Button;
